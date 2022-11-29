a11f784b08377db1fe50588cb410579e29498cbc
leffw <93302536+leffw@users.noreply.github.com>
2022-11-29 17:38:08 -0300

    Update lightning.py

--- backend/services/lightning.py
+++ backend/services/lightning.py
@@ -1,7 +1,45 @@
-from configs import LNBITS_HOST, LNBITS_MAIN_WALLET_INVOICE_KEY, LNBITS_MAIN_WALLET_ADMIN_KEY, LNBITS_WEBHOOK_URL
+from configs import PATH, LNBITS_HOST, LNBITS_BASE_URL, LNBITS_MAIN_WALLET_INVOICE_KEY, LNBITS_MAIN_WALLET_ADMIN_KEY, LNBITS_WEBHOOK_URL
+from os.path import exists
 from lnbits import Lnbits
+from json import loads, load, dump
+from re import search
+
+import requests
+import logging
+import sys
+
+if not (LNBITS_MAIN_WALLET_ADMIN_KEY):
+    if exists(PATH + "/data/wallet.json"):
+        wallet = load(open(PATH + "/data/wallet.json"))
+    else:
+        wallet = {}
+
+    if not wallet:
+        try:
+            location = requests.get(f"{LNBITS_BASE_URL}/wallet?nme=default", allow_redirects=False).headers["Location"]
+            wallet_keys = loads(search(r"window\.wallet = ({.*});", requests.get(f"{LNBITS_BASE_URL}{location}").text).group(1))
+        except:
+            logging.critical("Unable to connect with Lnbits.")
+            logging.critical("Exit")
+            sys.exit(0)
+
+        LNBITS_MAIN_WALLET_ADMIN_KEY = wallet_keys["adminkey"]
+        LNBITS_MAIN_WALLET_INVOICE_KEY = wallet_keys["inkey"]    
+        with open(PATH + "/data/wallet.json", "w") as w:
+            wallet = {"LNBITS_MAIN_WALLET_ADMIN_KEY": LNBITS_MAIN_WALLET_ADMIN_KEY, "LNBITS_MAIN_WALLET_INVOICE_KEY": LNBITS_MAIN_WALLET_INVOICE_KEY}
+            dump(wallet, w)
+    else:
+        LNBITS_MAIN_WALLET_ADMIN_KEY = wallet["LNBITS_MAIN_WALLET_ADMIN_KEY"]
+        LNBITS_MAIN_WALLET_INVOICE_KEY = wallet["LNBITS_MAIN_WALLET_INVOICE_KEY"]
 
 lnbits = Lnbits(admin_key=LNBITS_MAIN_WALLET_ADMIN_KEY, invoice_key=LNBITS_MAIN_WALLET_INVOICE_KEY, url=LNBITS_HOST)
+try:
+    if (lnbits.get_wallet().get("detail")):
+        raise Exception("Wallet does not exist.")
+except:
+    logging.critical("Unable to connect with Lnbits.")
+    logging.critical("Exit")
+    sys.exit(0)
 
 def create_invoice(amount: int, memo="", expiry=86400) -> dict:
     """Create a new lightning invoice containing metadata that will be used in 
@@ -10,8 +48,8 @@ def create_invoice(amount: int, memo="", expiry=86400) -> dict:
 
     invoice = lnbits.create_invoice(amount, memo=memo, webhook=LNBITS_WEBHOOK_URL)
     if not invoice.get("payment_hash"):
-        return {"message": "There was a problem trying to create a new invoice."}
-        
+        return {"message": invoice}
+    
     # Get the hash payment.
     payment_hash = invoice["payment_hash"]
 
